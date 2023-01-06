62154778b8e8cec221b8ff5db3196c9d480e6b41
aly2191 <ad2191@bioresource.nihr.ac.uk>
2023-01-06 14:55:27 +0000

    Added second screen, currently added phone and start of activity type selection

---
+++ CRM/Nbrprojectvolunteerlist/Form/Search/StageTwoScreen.mgd.php
@@ -0,0 +1,17 @@
+<?php
+// This file declares a managed database record of type "CustomSearch".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
+return [
+  [
+    'name' => 'CRM_Nbrprojectvolunteerlist_Form_Search_StageTwoScreen',
+    'entity' => 'CustomSearch',
+    'params' => [
+      'version' => 3,
+      'label' => 'StageTwoScreen',
+      'description' => 'StageTwoScreen (nbrprojectvolunteerlist)',
+      'class_name' => 'CRM_Nbrprojectvolunteerlist_Form_Search_StageTwoScreen',
+    ],
+  ],
+];
